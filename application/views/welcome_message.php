6ec9b2f1e5e8d5770f69f8cb96256364815eb221
bpierro <bc.pierro@ig.com.br>
2014-04-02 14:40:20 -0300

    adicionando arquivos tcc

---
+++ application/views/welcome_message.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>x</title>
+	<link href="../tccign/application/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="../tccign/application/css/css.css" rel="stylesheet" type="text/css" />
+<body style="background:#B5C1FF">
+<header>
+	<div class="navbar navbar-inverse" style="background-color:#26457F">
+		<button type="button" class="btn btn-default navbar-btn" style="background-color:#26457F;color:#FFFAFA;  float: right; margin-right: 2%;"><span class="glyphicon glyphicon-user"> Login</button>
+	</div>
+	<div class="headMenu">
+		<img src="../tccign/application/img/logo.png" style="height:80px; width:300px">
+	</div>
+</header>
+<div class="panel panel-default">
+  <div class="panel-body">
+
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+</div>
+
+</body>
+</html>
